da931a59b21441232e420e75952a58c2763dc922
DJYisLuck <duxy1118@163.com>
2019-05-06 15:55:35 +0800

    都改为正确

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -2,7 +2,7 @@ class TestLogin:
 
     def test_login1(self):
         print("test_login1")
-        assert 0
+        assert 1
 
     def test_login2(self):
         print("test_login2")
@@ -10,7 +10,7 @@ class TestLogin:
 
     def test_login3(self):
         print("test_login3")
-        assert 0
+        assert 1
 
     def test_login4(self):
         print("test_login4")
