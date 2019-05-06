2f9964f704e3205f985d465d2fdcb7988357ecc6
DJYisLuck <duxy1118@163.com>
2019-05-06 15:55:24 +0800

    第一个改为错误，第四个改为正确

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -2,7 +2,7 @@ class TestLogin:
 
     def test_login1(self):
         print("test_login1")
-        assert 1
+        assert 0
 
     def test_login2(self):
         print("test_login2")
@@ -14,4 +14,4 @@ class TestLogin:
 
     def test_login4(self):
         print("test_login4")
-        assert 0
+        assert 1
